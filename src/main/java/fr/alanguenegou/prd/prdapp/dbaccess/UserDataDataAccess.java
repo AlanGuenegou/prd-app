bf38e8db7f1aa65bae3b585bdbb6cad780d02e63
Alan Guenegou <alan.guenegou@hotmail.fr>
2022-01-06 19:06:01 +0100

    initial commit

---
+++ src/main/java/fr/alanguenegou/prd/prdapp/dbaccess/UserDataDataAccess.java
@@ -0,0 +1,27 @@
+package fr.alanguenegou.prd.prdapp.dbaccess;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@SpringBootApplication(scanBasePackages = {"fr.alanguenegou.prd.prdapp"})
+public class UserDataDataAccess implements CommandLineRunner {
+
+    private final static Logger log = LoggerFactory.getLogger(GraphDataAccess.class);
+
+    @Autowired
+    @Qualifier("jdbcUserData")
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        // code où on  vient récupérer les données utilisateur ??
+    }
+}
--- src/main/java/fr/alanguenegou/prd/prdapp/dbconfig/GraphDataSourceConfig.java
+++ src/main/java/fr/alanguenegou/prd/prdapp/dbconfig/GraphDataSourceConfig.java
@@ -1,10 +1,13 @@
 package fr.alanguenegou.prd.prdapp.dbconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -16,5 +19,11 @@ public class GraphDataSourceConfig {
     @Primary
     public DataSource graphDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "jdbcGraph")
+    @Autowired
+    public JdbcTemplate graphJdbcTemplate(@Qualifier("graphDataSource") DataSource graphDataSource) {
+        return new JdbcTemplate(graphDataSource);
     }
 }
